a5a5d4d7babff6de9f95462ac6d3e524e96d2242
Erl Jocson <jocsonerl@gmail.com>
2022-02-12 23:31:41 +0800

    Implemented LoginManager

--- SocialMediaProject/__init__.py
+++ SocialMediaProject/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder="template", static_folder="static")
 admin = Admin(app, name='Social Media', template_mode='bootstrap3')
+login_manager = LoginManager(app)
 
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+login_manager.login_view = 'login'
+login_manager.login_message = "Please login to access the page."
+login_manager.login_message_category = 'success'
 
 from .AppRoutes import Main, Auth, Posting
