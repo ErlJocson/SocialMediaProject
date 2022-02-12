3acba3e1024cd1de9a9a208ca7a5bb6012b6b764
Erl Jocson <jocsonerl@gmail.com>
2022-02-12 23:19:13 +0800

    New commit

--- SocialMediaProject/__init__.py
+++ SocialMediaProject/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_admin import Admin
 
 app = Flask(__name__, template_folder="template", static_folder="static")
+admin = Admin(app, name='Social Media', template_mode='bootstrap3')
+
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 from .AppRoutes import Main, Auth, Posting
