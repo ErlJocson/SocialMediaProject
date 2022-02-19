748d274cf7b39fc5564cd845d57667351b149aee
Erl Jocson <jocsonerl@gmail.com>
2022-02-19 13:52:45 +0800

    added a comment form

--- SocialMediaProject/Database/manage_post.py
+++ SocialMediaProject/Database/manage_post.py
@@ -29,13 +29,31 @@ def get_posts():
     cur.execute(
         """
             SELECT posts.id, posts.content, posts.date, posts.likes, posts.user_id, users.first_name, users.last_name 
-            FROM posts INNER JOIN users on posts.user_id=users.id
+            FROM posts INNER JOIN users ON posts.user_id=users.id
+            ORDER BY posts.date DESC
         """
     )
     posts = cur.fetchall()
     conn.close()
 
     return posts
+
+def get_post_by_id(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+            SELECT posts.id, posts.content, posts.date, posts.likes, posts.user_id, users.first_name, users.last_name
+            FROM posts INNER JOIN users ON posts.user_id = users.id
+            WHERE posts.id=?
+        """,
+        [id]
+    )
+
+    post = cur.fetchone()
+    conn.close()
+    return post
 
 def add_a_like(post_id):
     conn = get_db_connection()
