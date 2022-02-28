1d3931e7a186ad23fab398b242162abf8ad7da23
Erl Jocson <jocsonerl@gmail.com>
2022-02-28 20:37:26 +0800

    deleted some files

--- SocialMediaProject/Database/database.py
+++ SocialMediaProject/Database/database.py
@@ -3,87 +3,69 @@ import sqlite3
 conn = sqlite3.connect('social.db')
 cur = conn.cursor()
 
-# cur.execute(
-#     """
-#         CREATE TABLE users(
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             first_name TEXT NOT NULL,
-#             last_name TEXT NOT NULL,
-#             middle_name TEXT,
-#             email TEXT NOT NULL UNIQUE,
-#             profile_pic TEXT,
-#             password TEXT NOT NULL
-#         )
-#     """
-# )
-
-# cur.execute(
-#     """
-#         CREATE TABLE posts(
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             content TEXT NOT NULL,
-#             date TIMESTAMP DEFAULT (DATETIME('now')),
-#             user_id INTEGER NOT NULL,
-#             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-#         )
-#     """
-# )
-
-# cur.execute(
-#     """
-#         CREATE TABLE likes(
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             user_id INTEGER NOT NULL,
-#             post_id INTEGER NOT NULL,
-#             is_like INTEGER NOT NULL DEFAULT 0,
-#             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-#             FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
-#         )
-#     """
-# )
-
-# cur.execute(
-#     """
-#         CREATE TABLE comments(
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             content TEXT NOT NULL,
-#             date TIMESTAMP DEFAULT (DATETIME('now')),
-#             post_id INTEGER NOT NULL,
-#             user_id INTEGER NOT NULL,
-#             FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-#             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-#         )
-#     """
-# )
-
-# cur.execute(
-#     """
-#         CREATE TABLE following(
-#             user_id INTEGER NOT NULL,
-#             followed INTEGER NOT NULL,
-#             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-#             FOREIGN KEY (followed) REFERENCES users(id) ON DELETE CASCADE
-#         );
-#     """
-# )
-
-
-
-# cur.execute(
-#         """
-#             SELECT following.user_id, users.first_name, users.last_name
-#             FROM following INNER JOIN users ON following.user_id=users.id
-#             WHERE following.followed=?
-#         """,
-#         [1]
-#     )
-
-
 cur.execute(
-    'select * from following'
+    """
+        CREATE TABLE users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            middle_name TEXT,
+            email TEXT NOT NULL UNIQUE,
+            profile_pic TEXT,
+            password TEXT NOT NULL
+        )
+    """
 )
 
-print(cur.fetchall())
+cur.execute(
+    """
+        CREATE TABLE posts(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT NOT NULL,
+            date TIMESTAMP DEFAULT (DATETIME('now')),
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+    """
+)
+
+cur.execute(
+    """
+        CREATE TABLE likes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            post_id INTEGER NOT NULL,
+            is_like INTEGER NOT NULL DEFAULT 0,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+            FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+        )
+    """
+)
+
+cur.execute(
+    """
+        CREATE TABLE comments(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT NOT NULL,
+            date TIMESTAMP DEFAULT (DATETIME('now')),
+            post_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+    """
+)
+
+cur.execute(
+    """
+        CREATE TABLE following(
+            user_id INTEGER NOT NULL,
+            followed INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+            FOREIGN KEY (followed) REFERENCES users(id) ON DELETE CASCADE
+        );
+    """
+)
 
 conn.commit()
 conn.close()
